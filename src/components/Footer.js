a9a85df47924d89646b3ac82c9cdd3edab86b8f9
Matt <matt.chan120@gmail.com>
2020-07-02 17:13:56 -0400

    readme and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,16 +9,16 @@ function Footer() {
         <p>Copyright Matthew Chan 2020. All Rights Reserved</p>
         <div className = 'socialMedia'>
           <a href = "https://www.facebook.com">
-            <img src = {require('../images/facebook.png')} alt = ' '/>
+            <img src = {require('../images/facebook.svg')} alt = ' '/>
           </a>
           <a href = "https://www.instagram.com">
-            <img src = {require('../images/instagram.png')} alt = ' '/>
+            <img src = {require('../images/instagram.svg')} alt = ' '/>
           </a>
           <a href = "https://www.twitter.com">
-            <img src = {require('../images/twitter.png')} alt = ' '/>
+            <img src = {require('../images/twitter.svg')} alt = ' '/>
           </a>
           <a href = "https://www.linkedin.com">
-            <img src = {require('../images/linkedin.png')} alt = ' '/>
+            <img src = {require('../images/linkedin.svg')} alt = ' '/>
           </a>
         </div>
     </footer>
