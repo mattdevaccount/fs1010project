10793d3d6a3ea8efd8c49c9ba55001d8fb75c442
Matt <matt.chan120@gmail.com>
2020-07-01 20:21:20 -0400

    contact form submission

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,87 @@
-import React, {Fragment, useState} from 'react';
+import React, {useState} from 'react';
 import '../styles/App.scss';
 import '../styles/contactStyles.scss';
-import { Form, Input, TextArea, Button } from 'semantic-ui-react';
+//import { Form, Input, TextArea, Button } from 'semantic-ui-react';
 import Footer from './Footer';
-//import { Form, FormGroup, Col, Input, Label, Button, Container, CardBody, Card, CardText } from 'reactstrap'
+import { Form, FormGroup, Col, Input, Label, Button, Container, CardBody, Card, CardText } from 'reactstrap'
 
-function Contact() {
+// function Contact() {
+//     const [name, setName] = useState("")
+//     const [email, setEmail] = useState("")
+//     const [phoneNumber, setPhoneNumber] = useState("")
+//     const [content, setContent] = useState("")
+
+//     const formSubmit = async event => {
+//         event.preventDefault()
+//         const response = await fetch('http://localhost:4000/contact_form/entries', {
+//             method: 'POST',
+//             headers: {
+//                 'Accept': 'application/json',
+//                 'Content-Type': 'application/json'
+//               },
+//             body: JSON.stringify({name, email, phoneNumber, content})
+//         })
+//         const payload = await response.json()
+//         if (response.status >= 400) {
+//             alert(`Oops! Error: ${payload.message} for fields: ${payload.invalid.join(",")}`)
+//         } else {
+//             alert(`Congrats! Submission submitted with id: ${payload.id}`)
+//         }
+//     }
+
+//   return (
+//     <Fragment>
+//       <h1>Contact Me !</h1>
+//       <p>If you'd like to reach out to me, for collaboration, for projects, whatever, feel free to provide me with your info and I'll get back to you within 24 hours!</p>
+//         <Form.Group onSubmit={formSubmit}>
+//         <Container>
+//           <Form.Field
+//                   id='name-field'
+//                   control={Input}
+//                   label='Name'
+//                   placeholder='Enter your name here!'
+//                   required value={name}
+//                   onChange = {e => setName(e.target.value)}
+//                   />
+//                   <Form.Field
+//                   id='email-field'
+//                   control={Input}
+//                   label='Email'
+//                   placeholder='Your email here!'
+//                   required value={email}
+//                   onChange = {e => setEmail(e.target.value)}
+//                   />
+//                   <Form.Field
+//                   id='phone-field'
+//                   control={Input}
+//                   label='Phone Number'
+//                   placeholder='Your contact number here!'
+//                   required value={phoneNumber}
+//                   onChange = {e => setPhoneNumber(e.target.value)}
+//                   />
+//                   <Form.Field
+//                   id='message-field'
+//                   control={TextArea}
+//                   label='Message'
+//                   placeholder='Comments, ideas, or a bit about yourself!'
+//                   required value={content}
+//                   onChange = {e => setContent(e.target.value)}
+//                   />
+
+//                   <Button inverted animated='fade'>
+//                     <Button.Content visible>Submit</Button.Content>
+//                     <Button.Content hidden>We'll be in touch!</Button.Content>
+//                   </Button>
+//                   </Container>
+//           </Form.Group>
+    
+
+//     <Footer />
+//     </Fragment>
+
+
+
+const Contact = () => {
     const [name, setName] = useState("")
     const [email, setEmail] = useState("")
     const [phoneNumber, setPhoneNumber] = useState("")
@@ -29,54 +105,48 @@ function Contact() {
         }
     }
 
-  return (
-    <Fragment>
-      <h1>Contact Me !</h1>
-      <p>If you'd like to reach out to me, for collaboration, for projects, whatever, feel free to provide me with your info and I'll get back to you within 24 hours!</p>
-        <Form.Group onSubmit={formSubmit}>
-          <Form.Field
-                  id='name-field'
-                  control={Input}
-                  label='Name'
-                  placeholder='Enter your name here!'
-                  required value={name}
-                  onChange = {e => setName(e.target.value)}
-                  />
-                  <Form.Field
-                  id='email-field'
-                  control={Input}
-                  label='Email'
-                  placeholder='Your email here!'
-                  required value={email}
-                  onChange = {e => setEmail(e.target.value)}
-                  />
-                  <Form.Field
-                  id='phone-field'
-                  control={Input}
-                  label='Phone Number'
-                  placeholder='Your contact number here!'
-                  required value={phoneNumber}
-                  onChange = {e => setPhoneNumber(e.target.value)}
-                  />
-                  <Form.Field
-                  id='message-field'
-                  control={TextArea}
-                  label='Message'
-                  placeholder='Comments, ideas, or a bit about yourself!'
-                  required value={content}
-                  onChange = {e => setContent(e.target.value)}
-                  />
+    return (
+        <Container>
+            <Card className="text-white bg-secondary my-5 py-4 text-center">
+                <CardBody>
+                    <CardText className="text-white m-0">If you'd like to reach out to me, for collaboration, for projects, whatever, feel free to provide me with your info and I'll get back to you within 24 hours!</CardText>
+                </CardBody>
+            </Card>
+            <Form className="my-5" onSubmit={formSubmit}>
+                <FormGroup row>
+                    <Label for="emailEntry" sm={2}>Email</Label>
+                    <Col sm={10}>
+                    <Input type="email" name="email" id="emailEntry" placeholder="Enter email to contact"  required value={email} onChange={e => setEmail(e.target.value) }/>
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                    <Label for="phoneEntry" sm={2}>Phone Number</Label>
+                    <Col sm={10}>
+                    <Input type="phone" name="phone" id="phoneEntry" placeholder="Enter phone number" value={phoneNumber} onChange={e => setPhoneNumber(e.target.value)}/>
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                    <Label for="nameEntry" sm={2}>Full Name</Label>
+                    <Col sm={10}>
+                    <Input type="name" name="name" id="nameEntry" placeholder="Enter your full name" required value={name} onChange={e => setName(e.target.value)}/>
+                    </Col>
+                </FormGroup>
 
-                  <Button inverted animated='fade'>
-                    <Button.Content visible>Submit</Button.Content>
-                    <Button.Content hidden>We'll be in touch!</Button.Content>
-                  </Button>
-          </Form.Group>
+                <FormGroup row>
+                    <Label for="messageEntry" sm={2}>Message</Label>
+                    <Col sm={10}>
+                    <Input type="textarea" name="text" id="messageEntry" required value={content} onChange={e => setContent(e.target.value)}/>
+                    </Col>
+                </FormGroup>
+                <FormGroup check row>
+                    <Col sm={{ size: 10, offset: 2 }}>
+                    <Button color="success">Submit</Button>
+                    </Col>
+                </FormGroup>
+            </Form>
+        <Footer />
+        </Container>
+      )
+    }
 
-
-    <Footer />
-    </Fragment>
-  );
-}
-
-export default Contact;
+  export default Contact
